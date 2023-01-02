883f871770cb2babb396d4cfcc666bc4ffa1d449
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-01-02 22:28:06 +0100

    changed variables names

--- js/script.js
+++ js/script.js
@@ -1,71 +1,70 @@
-const hideShowText__foodSection = (sectionFood__buttonElement) => {
-    if (sectionFood__buttonElement.innerText === "Hide") {
-        sectionFood__buttonElement.innerText = "Show";
+const hideShowTextFoodSection = (sectionFoodButtonElement) => {
+    if (sectionFoodButtonElement.innerText === "Hide") {
+        sectionFoodButtonElement.innerText = "Show";
     } else {
-        sectionFood__buttonElement.innerText = "Hide";
+        sectionFoodButtonElement.innerText = "Hide";
     }
 };
 
-const hideShowText__cultureSection = (sectionCulture__buttonElement) => {
-    if (sectionCulture__buttonElement.innerText === "Hide") {
-        sectionCulture__buttonElement.innerText = "Show";
+const hideShowTextCultureSectionElement = (sectionCultureButtonElement) => {
+    if (sectionCultureButtonElement.innerText === "Hide") {
+        sectionCultureButtonElement.innerText = "Show";
     } else {
-        sectionCulture__buttonElement.innerText = "Hide";
+        sectionCultureButtonElement.innerText = "Hide";
     }
 };
 
-const hideShowText__travelSection = (sectionTravel__buttonElement) => {
-    if (sectionTravel__buttonElement.innerText === "Hide") {
-        sectionTravel__buttonElement.innerText = "Show";
+const hideShowTextTravelSectionElement = (sectionTravelButtonElement) => {
+    if (sectionTravelButtonElement.innerText === "Hide") {
+        sectionTravelButtonElement.innerText = "Show";
     } else {
-        sectionTravel__buttonElement.innerText = "Hide";
+        sectionTravelButtonElement.innerText = "Hide";
     }
 };
 
 
 const init = () => {
 
-    const sectionFood__buttonElement = document.querySelector(".js-show-hideSectionFoodButton");
+    const sectionFoodButtonElement = document.querySelector(".js-show-hideSectionFoodButton");
     const foldableContainerFoodElement = document.querySelector(".js-foldableFoodContainer");
 
 
-    sectionFood__buttonElement.addEventListener("click", () => {
+    sectionFoodButtonElement.addEventListener("click", () => {
         foldableContainerFoodElement.classList.toggle("section__foldableContainer--hide");
 
-        sectionFood__buttonElement.addEventListener("click", hideShowText__foodSection);
+        sectionFoodButtonElement.addEventListener("click", hideShowTextFoodSection);
 
-        hideShowText__foodSection(sectionFood__buttonElement);
+        hideShowTextFoodSection(sectionFoodButtonElement);
 
     });
 
-    const sectionCulture__buttonElement = document.querySelector(".js-show-hideSectionCultureButton");
+    const sectionCultureButtonElement = document.querySelector(".js-show-hideSectionCultureButton");
     const foldableContainerCultureElement = document.querySelector(".js-foldableCultureContainer");
 
-    sectionCulture__buttonElement.addEventListener("click", () => {
+    sectionCultureButtonElement.addEventListener("click", () => {
         foldableContainerCultureElement.classList.toggle("section__foldableContainer--hide");
 
 
-        sectionCulture__buttonElement.addEventListener("click", hideShowText__cultureSection);
+        sectionCultureButtonElement.addEventListener("click", hideShowTextCultureSectionElement);
 
 
-        hideShowText__cultureSection(sectionCulture__buttonElement);
+        hideShowTextCultureSectionElement(sectionCultureButtonElement);
 
     });
 
 
-    const sectionTravel__buttonElement = document.querySelector(".js-show-hideSectionTravelButton");
+    const sectionTravelButtonElement = document.querySelector(".js-show-hideSectionTravelButton");
     const foldableContainerTravelElement = document.querySelector(".js-foldableTravelContainer");
 
-    sectionTravel__buttonElement.addEventListener("click", () => {
+    sectionTravelButtonElement.addEventListener("click", () => {
         foldableContainerTravelElement.classList.toggle("section__foldableContainer--hide");
 
 
-        sectionTravel__buttonElement.addEventListener("click", hideShowText__travelSection);
+        sectionTravelButtonElement.addEventListener("click", hideShowTextTravelSectionElement);
 
-        hideShowText__travelSection(sectionTravel__buttonElement);
+        hideShowTextTravelSectionElement(sectionTravelButtonElement);
 
     });
 };
 
 init();
-
